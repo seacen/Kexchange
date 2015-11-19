abf279134205e15ff358ae2f8d23b6aa3b7e7f67
Xichang(Seacen) Zhao <xichangzhao@outlook.com>
2015-11-20 00:39:32 +1100

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   before_create :setup_role
   before_create :setup_confirmed
 
-  validates_presence_of :email, :username, :city, :state, :country, :locale
+  validates_presence_of :email, :username, :city, :state, :country
   validates :email, format: { with: /(.+)@(.+).[a-z]{2,4}/, message: I18n.t('simple_form.error_notification.email.format') }
   validates :username, length: { minimum: 3 }
   validates :username, format: { with: /\A\w+\z/, message: I18n.t('simple_form.error_notification.username.format') }
